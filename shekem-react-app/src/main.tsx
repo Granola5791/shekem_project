92b9ab6238d1b5f12e564c12682e022211078458
Granola5791 <yuvalby1969@gmail.com>
2025-07-29 22:01:51 +0300

    implement routing with LoginPage and HomePage, add ErrorPage component, and create checkLogin utility for authentication

--- shekem-react-app/src/main.tsx
+++ shekem-react-app/src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import LoginPage from './pages/LoginPage.tsx'
+import HomePage from './pages/HomePage.tsx'
+import ErrorPage from './pages/ErrorPage.tsx'
+import {createBrowserRouter, RouterProvider } from "react-router-dom";
+import checkLogin from './utils/checkLogin.ts';
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+    loader: checkLogin,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ shekem-react-app/src/utils/checkLogin.ts
@@ -0,0 +1,18 @@
+export default async function HomeLoader() {
+  const res = await fetch('http://localhost:8081/api/check_login', {
+    method: 'GET',
+    credentials: 'include'
+  });
+  if (res.status === 401) {
+    console.log('Unauthorized');
+    throw new Response('Unauthorized', {
+      status: 302,
+      headers: { Location: '/login' },
+    });
+  }
+  else {
+    console.log('Authorized');
+  }
+
+  return null;
+}

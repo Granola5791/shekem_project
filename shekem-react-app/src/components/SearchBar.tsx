08d7651b66e2960320b33558e8f58799384c4369
Granola5791 <yuvalby1969@gmail.com>
2025-08-04 22:10:49 +0300

    created navbar and search bar in the frontend

---
+++ shekem-react-app/src/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+interface SearchBarProps {
+    onSearch: (searchInput: string) => void
+}
+
+
+const SearchBar = ( { onSearch }: SearchBarProps ) => {
+    const [searchInput, setSearchInput] = React.useState('');
+    return (
+        <Grid direction={'row'} sx={{ width: 'fit-content', height: 'fit-content', border: '1px solid black' }}>
+            <TextField
+                id="filled-search"
+                placeholder="Search"
+                type="search"
+                size="small"
+                onChange={(e) => setSearchInput(e.target.value)}
+                sx={{ width: '30vw', '& .MuiOutlinedInput-root': { '& fieldset': { border: 'none' } } }}
+            />
+            <IconButton type="submit" aria-label="search" onClick={() => onSearch(searchInput)}>
+                <SearchIcon />
+            </IconButton>
+        </Grid>
+
+    )
+}
+
+export default SearchBar
--- shekem-react-app/src/pages/HomePage.tsx
+++ shekem-react-app/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
 import React, { use, useEffect } from 'react'
-import { Container } from '@mui/material';
+import { AppBar, Container, IconButton } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import ItemCard from '../components/ItemCard';
 import { parse, stringify } from 'yaml'
 import { isUnauthorizedResponse } from '../utils/http.ts';
+import SearchBar from '../components/SearchBar.tsx';
 
 async function AddToCart(id: number, selectCount: number) {
     const res = await fetch('http://localhost:8081/api/add_to_cart', {
@@ -19,9 +22,14 @@ async function AddToCart(id: number, selectCount: number) {
     }
 }
 
+function Search(searchInput: string) {
+
+}
+
 type config = {
     add_to_cart_button: string
     shekel_symbol: string
+    search_bar_text: string
 }
 
 const HomePage = () => {
@@ -36,16 +44,112 @@ const HomePage = () => {
 
     return (
         <>
-            <Container sx={{ height: '100vh' }}>
-                <ItemCard
-                    id={1}
-                    itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
-                    price={100}
-                    moneySymbol={config.shekel_symbol}
-                    buttonText={config.add_to_cart_button}
-                    image='src/assets/item_example.jpg'
-                    AddToCart={AddToCart}
-                />
+            <AppBar position='fixed' sx={{ height: '15vh', bgcolor: 'rgba(255, 235, 19, 1)' }}>
+                <Grid container spacing={0} >
+                    <Grid container direction="row" alignItems={"center"}>
+                        <img
+                            src="./src/assets/caveret-logo.svg"
+                            alt="caveret-logo"
+                            style={{ height: '15vh', width: 'fit-content' }}
+                        />
+                        <SearchBar onSearch={Search} />
+                    </Grid>
+                </Grid>
+            </AppBar>
+            <Container sx={{ height: '80vh', marginTop: '20vh' }}>
+                <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />
+                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />
+                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />                    <ItemCard
+                        id={1}
+                        itemTitle={"lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"}
+                        price={100}
+                        moneySymbol={config.shekel_symbol}
+                        buttonText={config.add_to_cart_button}
+                        image='src/assets/item_example.jpg'
+                        AddToCart={AddToCart}
+                    />
+                </Grid>
             </Container >
         </>
     )

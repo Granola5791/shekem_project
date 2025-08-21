44b12069ac3ff26e2d28e6092ec4a161d3a44b0c
Granola5791 <yuvalby1969@gmail.com>
2025-08-21 19:41:20 +0300

    created types for constants.

--- shekem-react-app/src/utils/constants.ts
+++ shekem-react-app/src/utils/constants.ts
@@ -1,3 +1,43 @@
+export type HebrewConstants = {
+    add_to_cart_button: string;
+    shekel_symbol: string;
+    search_bar_text: string;
+    category_list_title: string;
+    user_errors: {
+        page_not_found: string;
+    };
+};
+
+export type BackendConstants = {
+    backend_address: string;
+    add_to_cart_api: string;
+    get_categories_api: string;
+    get_category_photo_api: string;
+
+    statuses: {
+        ok: string;
+        unauthorized: string;
+        not_found: string;
+        internal_server_error: string;
+    };
+
+    status_codes: {
+        ok: number;
+        unauthorized: number;
+        not_found: number;
+        internal_server_error: number;
+    };
+};
+
+export type GeneralConstants = {
+    errors: {
+        config_load_fail: string;
+        config_not_found: string;
+        category_load_fail: string;
+        category_load_not_found: string;
+    };
+};
+
 export function insertValuesToConstantStr(configString: string, ...values: number[]): string {
     let argsIndex = 0;
     let result = '';
@@ -14,4 +54,3 @@ export function insertValuesToConstantStr(configString: string, ...values: numbe
     }
     return result;
 }
-

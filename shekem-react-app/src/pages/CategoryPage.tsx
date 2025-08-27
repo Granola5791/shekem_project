047ab77677392cb3106ef0e7616c5c51215f2dce
Granola5791 <yuvalby1969@gmail.com>
2025-08-27 20:11:34 +0300

    added a navbar to category page.

--- shekem-react-app/src/pages/CategoryPage.tsx
+++ shekem-react-app/src/pages/CategoryPage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import NavBar from '../components/NavBar'
+import { useNavigation } from '../utils/navigation'
 
 const CategoryPage = () => {
+
+    const {
+        goToHome: GoToHome,
+        goToCart: GoToCart,
+        searchItems: SearchItems
+    } = useNavigation()
+
     return (
-        <div>CategoryPage</div>
+        <>
+            <NavBar
+                onSearch={SearchItems}
+                goToCart={GoToCart}
+                logoClick={GoToHome}
+            />            
+        </>
     )
 }
 

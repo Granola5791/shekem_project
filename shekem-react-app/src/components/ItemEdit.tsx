53f93d8f75f6b372d0289e9035a73dff16cc4027
Granola5791 <yuvalby1969@gmail.com>
2025-09-05 15:18:33 +0300

    created ItemEdit.tsx.

---
+++ shekem-react-app/src/components/ItemEdit.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import ImageUploader from './ImageUploader';
+
+interface ItemEditProps {
+    open: boolean
+    itemID?: number
+    itemIDLabel: string
+    itemTitle?: string
+    itemTitleLabel: string
+    price?: number
+    priceLabel: string
+    stock?: number
+    stockLabel: string
+    imageUrl?: string
+    imageLabel: string
+    onSubmit?: (itemID: number, itemTitle: string, price: number, stock: number, image: File) => void
+    submitButtonText?: string
+    onCancel?: () => void
+    cancelButtonText?: string
+
+}
+
+const ItemEdit = ({ open, itemID, itemIDLabel, itemTitle, itemTitleLabel, price, priceLabel, stock, stockLabel, imageUrl, imageLabel, onSubmit, submitButtonText, onCancel, cancelButtonText }: ItemEditProps) => {
+
+    const [image, setImage] = React.useState<File | null>(null);
+    const [imageUrlHook, setImageUrlHook] = React.useState<string>(imageUrl || '');
+    const [priceHook, setPriceHook] = React.useState<number>(price || 0);
+    const [stockHook, setStockHook] = React.useState<number>(stock || 0);
+    const [itemTitleHook, setItemTitleHook] = React.useState<string>(itemTitle || '');
+    const [itemIDHook, setItemIDHook] = React.useState<number>(itemID || 0);
+
+    const HandleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            const file = event.target.files[0];
+            const fileUrl = URL.createObjectURL(file);
+            setImage(file);
+            setImageUrlHook(fileUrl);
+        }
+    }
+
+    return (
+        <Dialog open={open} onClose={() => { setImage(null); setImageUrlHook(imageUrl || ''); }}>
+            <Stack spacing={2} padding={2}>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <label htmlFor="id">{itemIDLabel}:</label>
+                    <TextField id='id' placeholder={itemIDLabel} defaultValue={itemID} />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <label htmlFor="name">{itemTitleLabel}:</label>
+                    <TextField id='name' placeholder={itemTitleLabel} defaultValue={itemTitle} />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <label htmlFor="price">{priceLabel}:</label>
+                    <TextField id='price' placeholder={priceLabel} defaultValue={price} />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <label htmlFor="stock">{stockLabel}:</label>
+                    <TextField id='stock' placeholder={stockLabel} defaultValue={stock} />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <label>{imageLabel}:</label>
+                    <ImageUploader onChange={HandleImageUpload} fileUrl={imageUrlHook} />
+                </Box>
+            </Stack>
+
+            <Box gap={2} sx={{ display: 'flex', justifyContent: 'center', padding: 2 }}>
+                <Button variant='outlined' onClick={onCancel}>
+                    {cancelButtonText}
+                </Button>
+                <Button variant='contained' onClick={() => onSubmit?.(itemIDHook, itemTitleHook, priceHook, stockHook, image || new File([], ''))} sx={{ backgroundColor: 'rgb(239, 232, 26)' }}>
+                    {submitButtonText}
+                </Button>
+            </Box>
+        </Dialog>
+    )
+}
+
+export default ItemEdit

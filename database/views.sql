3e6b366673c83b0ea1450abd2e97a5dbd967b66e
Granola5791 <yuvalby1969@gmail.com>
2025-09-08 22:35:19 +0300

    create undeleted_items and undeleted_users views.

--- database/views.sql
+++ database/views.sql
@@ -3,3 +3,13 @@ CREATE VIEW recommended_items AS
     FROM items i
     JOIN cat_to_item ctoi ON i.item_id = ctoi.item_id
     WHERE ctoi.category_id = 1; -- 1 is the 'recommended' category
+
+CREATE OR REPLACE VIEW undeleted_items AS
+    SELECT *
+    FROM items
+    WHERE is_deleted = FALSE;
+
+CREATE OR REPLACE VIEW undeleted_users AS
+    SELECT *
+    FROM users
+    WHERE is_deleted = FALSE;

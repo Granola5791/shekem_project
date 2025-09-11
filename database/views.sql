85c1ca99f8d3cdd57038dbde24103bed622a9f6f
Granola5791 <yuvalby1969@gmail.com>
2025-09-11 19:20:01 +0300

    removed some useless code.

--- database/views.sql
+++ database/views.sql
@@ -1,9 +1,3 @@
-CREATE VIEW recommended_items AS
-    SELECT i.*
-    FROM items i
-    JOIN cat_to_item ctoi ON i.item_id = ctoi.item_id
-    WHERE ctoi.category_id = 1; -- 1 is the 'recommended' category
-
 CREATE OR REPLACE VIEW undeleted_items AS
     SELECT *
     FROM items
